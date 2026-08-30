e4d2d91836b4e7807710f05c960a9deaba6c8e5b
Tayyaba Zafar <tayyabazafar1200@gmail.com>
2026-08-30 15:16:37 +0500

    Integrate Tailwind CSS with Django and create superuser for admin access

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,8 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
-
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1']
+# NPM_BIN_PATH = '/usr/local/bin/npm'
+# for windows user
+# NPM_BIN_PATH = r"C:\Program File\etc\etc"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -48,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'myproject.urls'
--- myproject/urls.py
+++ myproject/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('core/', include('core.urls')),
+
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
